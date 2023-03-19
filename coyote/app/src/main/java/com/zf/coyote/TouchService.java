9af7eb6416f907e395cc1ecdef24b5ed99726985
zhaofeng256 <zhaofeng256@foxmail.com>
2023-03-19 15:28:16 +0800

    update
    Signed-off-by: zhaofeng256 <zhaofeng256@foxmail.com>

--- coyote/app/src/main/java/com/zf/coyote/TouchService.java
+++ coyote/app/src/main/java/com/zf/coyote/TouchService.java
@@ -2,25 +2,17 @@ package com.zf.coyote;
 
 import android.app.Instrumentation;
 import android.app.Service;
-import android.app.UiAutomation;
 import android.content.Intent;
-import android.os.Handler;
 import android.os.IBinder;
 import android.os.SystemClock;
-import android.text.method.Touch;
 import android.util.Log;
 import android.view.InputDevice;
-import android.view.InputEvent;
 import android.view.MotionEvent;
-
-import java.io.IOException;
 
 public class TouchService extends Service {
 
     public static final String TAG = TouchService.class.getSimpleName();
     static Instrumentation mInst;
-    static UiAutomation mUi;
-    static int points = 0;
     float[] move_forward = {330, 820};
     float[] origin_point = {743, 409};
 
@@ -47,10 +39,10 @@ public class TouchService extends Service {
     public void test_multi_touch() {
 
         Log.d(TAG,"test multi touch" + android.os.Process.myUid());
-        Point start1 = new Point(300,300);
-        Point start2 = new Point(400,400);
-        Point end1 = new Point(500,500);
-        Point end2 = new Point(600,600);
+        Point start1 = new Point(300, 300);
+        Point start2 = new Point(400, 600);
+        Point end1 = new Point(500, 300);
+        Point end2 = new Point(600, 600);
         performPinch(start1, end1, start2, end2);
 
     }
@@ -60,7 +52,7 @@ public class TouchService extends Service {
       up(x,y);
     }
 
-    public static void drag(float x1, float y1, float x2, float y2, float duration) throws InterruptedException {
+    public static void drag(float x1, float y1, float x2, float y2, float duration) {
         final int interval = 25;
         int steps = (int) (duration / interval + 1);
         float dx = (x2 - x1) / steps;
@@ -114,14 +106,14 @@ public class TouchService extends Service {
         mInst.sendPointerSync(event);
         event.recycle();
     }
-    public class Point {
+    public static class Point {
         float x;
         float y;
         public Point(float x, float y) {
             this.x = x;
             this.y = y;
         }
-    };
+    }
     private static void performPinch( Point startPoint1, Point startPoint2, Point endPoint1, Point endPoint2) {
         final int duration = 500;
         final long eventMinInterval = 10;
@@ -147,7 +139,7 @@ public class TouchService extends Service {
         properties[0] = pp1;
         properties[1] = pp2;
 
-        // Specify the coordinations of the two touch points
+        // Specify the coordination's of the two touch points
         // NOTE: you MUST set the pressure and size value, or it doesn't work
         MotionEvent.PointerCoords[] pointerCoords = new MotionEvent.PointerCoords[2];
         MotionEvent.PointerCoords pc1 = new MotionEvent.PointerCoords();
